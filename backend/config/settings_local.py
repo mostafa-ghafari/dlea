eeb087f525813c393065b16a6f86dd373e0ad4d3
Mostafa Ghaffari <farvimail@gmail.com>
2026-09-03 18:12:51 +0330

    comment: settings_local damn file!

--- backend/config/settings_local.py
+++ backend/config/settings_local.py
@@ -1,11 +1,11 @@
-from .settings import *
+# from .settings import *
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': 'db.sqlite3',
+#     }
+# }
 
-DEBUG = True
-DJANGO_DEBUG = True
+# DEBUG = True
+# DJANGO_DEBUG = True
